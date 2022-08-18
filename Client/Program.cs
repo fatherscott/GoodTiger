10ddb3a4d4d9960a699e72e7690e0705d114ed48
fatherscott <ini64@kakao.com>
2022-08-18 10:05:47 +0900

    change test logic

--- Client/Program.cs
+++ Client/Program.cs
@@ -16,10 +16,10 @@ namespace Client
         static async Task Main(string[] args)
         {
             List<Task> tasks = new List<Task>();
-            for (int i = 1; i < 9999999; i += 20)
+            for (int i = 1; i < 9999999; i += 1000)
             {
                 tasks.Clear();
-                for (int ii = 0; ii < 20; ii++)
+                for (int ii = 0; ii < 1000; ii++)
                 {
                     tasks.Add(Client.TestClient(i + ii));
                 }
